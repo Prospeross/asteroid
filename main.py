133b9aafbe55827a2559e8297eb2ac8c3fecac5c
Prospeross <prozisv13@gmail.com>
2025-04-01 18:52:02 +0200

    There is a triangle player in the middle

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -12,14 +14,30 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+
+
+#glowna petla gry
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen,(0,0,0))
+        screen.fill("black")
+
+        player = Player(x,y)
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
