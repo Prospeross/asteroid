4ae1454fd9b7276c299e85ec30d5380bfae59ff4
Prospeross <prozisv13@gmail.com>
2025-04-02 08:52:35 +0200

    Objects are moving, also you can drive

--- main.py
+++ main.py
@@ -6,36 +6,56 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
 
+    dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(x,y)
+    asteroid_field = AsteroidField()
+
 
 
 
 #glowna petla gry
     while True:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+
+        updatable.update(dt)
+
         screen.fill("black")
 
-        player = Player(x,y)
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
 
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
+        
 
 
 
